9f229a874e65f82770a5d1533b427d24cee26c24
Mef1s <msadoch09@gmail.com>
2024-05-14 02:49:52 +0200

    #1 Landing page, frontend
    Menu, większość zakładki 'odkryj'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import logo from './logoQ.png';
+import user from './user.png';
+import quit from './quit.png';
+import lookup from './lookup.png';
+import ico_1 from './Component 1.png';
+import ico_2 from './Component 2.png';
+import ico_3 from './Component 3.png';
+import ico_4 from './Component 4.png';
+import ico_5 from './Component 5.png';
+import ico_6 from './Component 6.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div id="menu">
+        <div id="menu-content">
+          <img src={logo} width={400} alt="Logo" />
+          <div id="menubutton-active">Odkrywaj</div>
+          <div id="menubutton">Quizy szkolne</div>
+          <div id="menubutton">Quizy społecznościowe</div>
+          <div id="menubutton">Moje quizy</div>
+          <div id="menubutton">Moja biblioteka</div>
+        </div>
+        <div id="profile">
+          <img src={user} width={50} style={{ borderRadius: "50%" }} alt="user-image" />
+          <div id="user-info">
+            <div id="user-name">Simp Hunter</div>
+            <div id="user-mail">Simp_Hunter69@gmail.com</div>
+          </div>
+          <div id="quit">
+            <img src={quit} width={50} style={{ borderRadius: "50%" }} alt="user-image" />
+          </div>
+        </div>
+      </div>
+
+
+
+        <div id="main">
+          <div id="title">Powiedz mi czego szukasz...</div>
+            <div id='lookup_area'>Szukaj Quizów..
+              <div id='lookup_image'>
+                <img src={lookup} width={50} style={{ borderRadius: "50%" }} alt="lookup-image" />
+              </div>
+            </div>
+            <div id='ico_area'>
+              <div id='main_ico'>
+              <img src={ico_1} alt="ico-button" />
+              </div>
+              <div id='main_ico'>
+              <img src={ico_2} alt="ico-button" />
+              </div>
+              <div id='main_ico'>
+              <img src={ico_3} alt="ico-button" />
+              </div>
+              <div id='main_ico'>
+              <img src={ico_4} alt="ico-button" />
+              </div>
+              <div id='main_ico'>
+              <img src={ico_5} alt="ico-button" />
+              </div>
+              <div id='main_ico'>
+              <img src={ico_6} alt="ico-button" />
+              </div>
+            </div>
+        </div>
+      </div>
   );
 }
+
 
 export default App;
